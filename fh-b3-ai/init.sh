8cdc1950227c61224a44cf88af14bd538f9e5282
Marco Auenhammer <marco.auenhammer@gmail.com>
2024-09-20 00:31:04 +0200

    refined container definitions

--- fh-b3-ai/init.sh
+++ fh-b3-ai/init.sh
@@ -6,7 +6,6 @@ echo "Executing ${CONTAINER_ID} specific setup:"
 
 echo "Installing rye..."
 curl -sSf https://rye.astral.sh/get | bash
-echo 'source "$HOME/.rye/env"' >> ~/.zshrc
-
 mkdir $ZSH_CUSTOM/plugins/rye
+echo 'source "$HOME/.rye/env"' >> ~/.zshrc
 rye self completion -s zsh > $ZSH_CUSTOM/plugins/rye/_rye
