c06716f15f5901d49a9fda96a885d5bf93682a30
Marco Auenhammer <marco.auenhammer@gmail.com>
2024-09-20 13:05:56 +0200

    added automatics fetching container specific init scripts

--- fh-b3-ai/init.sh
+++ fh-b3-ai/init.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-/bin/bash ../init.sh
-
-echo "Executing ${CONTAINER_ID} specific setup:"
-
-echo "Installing rye..."
-curl -sSf https://rye.astral.sh/get | bash
+# Install & configure rye
+echo "Setup rye..."
+curl -sSf https://rye.astral.sh/get | zsh
 mkdir $ZSH_CUSTOM/plugins/rye
-echo 'source "$HOME/.rye/env"' >> ~/.zshrc
+echo 'source "$HOME/.rye/env"' >> ~/.zshrc # This will fail if shell wasn't changed to ZSH
 rye self completion -s zsh > $ZSH_CUSTOM/plugins/rye/_rye
--- init.sh
+++ init.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-GITHUB_USERNAME="DrStrangeloovee"
+# Exit if outside of container
+
+if [ -z ${CONTAINER_ID+x} ]; then
+    echo "ERROR: This script is intended to be run inside of a container!"
+    exit 1
+fi
+
+github_user="DrStrangeloovee"
+repo_name="my-distroboxes"
+repo_url="https://raw.githubusercontent.com/${github_user}/${repo_name}/master"
 
 echo "Executing initial post container creation setup:"
 
@@ -11,7 +20,31 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 
 # Chezmoi init --apply
 echo "Fetching dotfiles..."
-chezmoi init --apply git@github.com:$GITHUB_USERNAME/dotfiles.git
+chezmoi init --apply git@github.com:$github_user/dotfiles.git
+
+# If needed run additional container specific setup
+if [ $CONTAINER_ID != "base" ]; then
+
+    script_url="${repo_url}/${CONTAINER_ID}/init.sh"
+    echo "Fetching script from: ${script_url}"
+
+    # Try to download the script
+    if curl --silent --fail "$script_url" --output "init_$CONTAINER_ID.sh"; then
+        echo "Script $script_name.sh downloaded successfully."
+
+        # Set the script executable
+        chmod +x "init_$CONTAINER_ID.sh"
+
+        # Execute it
+        echo "Executing the congtainer specific setup..."
+        ./"init_$CONTAINER_ID.sh"
+
+        # Remove it after execution
+        rm ./"init_${CONTAINER_ID}.sh"
+    else
+        echo "Script not found at $script_url. Skipping execution."
+    fi
+fi
 
 echo "To finish the setup you have to re-enter the container!"
 
